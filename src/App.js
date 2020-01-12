00f90e12f72cbf1d509785466cd2fe6fdf598406
Vicky <vickykumar538@gmail.com>
2020-01-12 12:26:30 +0530

    autocompleted with useEffect

--- src/App.js
+++ src/App.js
@@ -1,26 +1,71 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import Autocomplete from "./autocomplete";
 
-function App() {
+import "./App.css";
+
+const App = () => {
+  const APP_ID = "712a2136";
+  const APP_KEY = "08675e5bf3c171e76ce515288d819ddc";
+
+  const [autoCompleteData, setAutoComplete] = useState([]);
+  const [search, setSearch] = useState("");
+
+  const [query, setQuery] = useState("chicken");
+
+  const getSearch = e => {
+    e.preventDefault();
+    setQuery(search);
+    setSearch("");
+    console.log(search);
+  };
+
+  const updateSearch = e => {
+    setSearch(e.target.value);
+    console.log(search);
+    // console.log(search);
+    // console.log(e.target.value);
+    // if (search.length > 2) {
+    // console.log("auto comeplete called");
+    //getAutoComplete();
+    // }
+  };
+
+  useEffect(() => {
+    getAutoComplete();
+  }, [search]);
+
+  const getAutoComplete = async () => {
+    console.log("search value in autocomplete", search);
+
+    const respone = await fetch(
+      `http://localhost:61092/api/food?FoodName=${search}`
+    );
+    const data = await respone.json();
+    setAutoComplete(data);
+    console.log(data);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={getSearch} className="search-form">
+        <input
+          type="text"
+          value={search}
+          onChange={updateSearch}
+          className="search-bar"
+        />
+
+        <button type="submit" className="search-button">
+          Search
+        </button>
+      </form>
+      <div>
+        {autoCompleteData.map(a => (
+          <Autocomplete key={a.id} title={a.Name} />
+        ))}
+      </div>
     </div>
   );
-}
+};
 
 export default App;
